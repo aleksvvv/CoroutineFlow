3d775334ff697fbafc7a8c8a2a264cf381bc11c2
aleksvvv <adm-shop-001@yandex.ru>
2023-04-18 09:25:22 +0300

    merge 1

--- app/src/main/java/com/bignerdranch/android/coroutineflow/crypto_app/CryptoViewModel.kt
+++ app/src/main/java/com/bignerdranch/android/coroutineflow/crypto_app/CryptoViewModel.kt
@@ -10,18 +10,22 @@ class CryptoViewModel : ViewModel() {
 
     private val repository = CryptoRepository
 
+private val loadingFlow = MutableSharedFlow<State>()
 
+private val fromRepository =repository.getCurrencyList()
+    .filter { it.isNotEmpty() }
+    .map { State.Content(currencyList = it) as State }
+    .onStart {
+        Log.d("CryptoViewModel", "Started")
+        emit(State.Loading)
+    }
 
-    val state: Flow<State> = repository.getCurrencyList()
-        .filter { it.isNotEmpty() }
-        .map { State.Content(currencyList = it) as State }
-        .onStart {
-            Log.d("CryptoViewModel", "Started")
-            emit(State.Loading)
-        }
+    val state: Flow<State> = merge(loadingFlow,fromRepository)
+
 
     fun refreshList() {
         viewModelScope.launch {
+            loadingFlow.emit(State.Loading)
             repository.refresh()
         }
     }
